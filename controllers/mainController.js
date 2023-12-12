6ddf3a46a5abcf2a703b0a8172cb331010b7a6fb
Lacus2611 <jmllacu26@yahoo.com.ar>
2023-12-12 11:06:38 -0300

    comienzo del proyecto

---
+++ controllers/mainController.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../products.json');
+
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const mainController = {
+    home: (req,res)=>{
+
+      res.render('home',{ products }); 
+	
+    } 
+}
+
+module.exports = mainController;
